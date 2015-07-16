a74787de1637e26da41e0b6cbdd0d9d7a524979e
alexmanly <am@chef.io>
2015-07-16 02:51:19 +0000

    changed hosts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,13 @@
 default['base-oracle-node']['hostname'] = 'oracledb1'
+default['base-oracle-node']['hosts'] =  {
+	'chefserver' => '10.0.0.10', 
+	'chefanalytics' => '10.0.0.20',
+	'centosweb01' => '10.0.0.30',
+	'centosweb02' => '10.0.0.40',
+	'loadbalancer' => '10.0.0.50',
+	'chefdk' => '10.0.0.60',
+	node['base-oracle-node']['hostname'] => '10.0.0.90'
+}
 default['base-oracle-node']['partition_size'] = '2G'
 default['base-oracle-node']['mount_point'] = '/opt/oracle'
 default['base-oracle-node']['device_id'] = '/dev/xvde2'
--- recipes/setup-hosts.rb
+++ recipes/setup-hosts.rb
@@ -17,101 +17,12 @@ hostsfile_entry '127.0.0.1' do
   action    :update
 end
 
-hostsfile_entry '10.0.0.10' do
-  hostname  'chefserver'
-  unique    true
-  aliases   ['chefserver.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '10.0.0.20' do
-  hostname  'chefanalytics'
-  unique    true
-  aliases   ['chefanalytics.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '10.0.0.30' do
-  hostname  'centosweb01'
-  unique    true
-  aliases   ['centosweb01.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '10.0.0.40' do
-  hostname  'centosweb02'
-  unique    true
-  aliases   ['centosweb02.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '10.0.0.50' do
-  hostname  'loadbalancer'
-  unique    true
-  aliases   ['loadbalancer.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '10.0.0.60' do
-  hostname  'chefdk'
-  unique    true
-  aliases   ['chefdk.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '10.0.0.90' do
-  hostname  node['base-oracle-node']['hostname']
-  unique    true
-  aliases   [node['base-oracle-node']['hostname'] + '.local']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry '::1' do
-  hostname  'ip6-localhost'
-  unique    true
-  aliases   ['ip6-loopback']
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry 'fe00::0' do
-  hostname  'ip6-localnet'
-  unique    true
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry 'ff00::0' do
-  hostname  'ip6-mcastprefix'
-  unique    true
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry 'ff02::1' do
-  hostname  'ip6-allnodes'
-  unique    true
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry 'ff02::2' do
-  hostname  'ip6-allrouters'
-  unique    true
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
-end
-
-hostsfile_entry 'ff02::3' do
-  hostname  'ip6-allhosts'
-  unique    true
-  comment   'Append by Recipe base-oracle-db::setup-hostsfile'
-  action    :create
+node['base-oracle-node']['hosts'].each do |host, ip|
+  hostsfile_entry ip do
+    hostname  host
+    unique    true
+    aliases   ["#{host}.local"]
+    comment   'Append by Recipe base-oracle-db::setup-hostsfile'
+    action    :create
+  end
 end
