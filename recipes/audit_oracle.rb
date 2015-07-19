bcf5f05eafb4050d1d416fc81b28e9a844f15f82
alexmanly <am@chef.io>
2015-07-19 23:00:27 +0000

    Added audit recipe

--- recipes/audit_oracle.rb
+++ recipes/audit_oracle.rb
@@ -1,14 +1,16 @@
-control_group "System Audit" do
-  iptables_command='chkconfig --list iptables'
-  describe command(iptables_command) do
-    its(:stdout) { should match /^iptables.+:on/ }
-  end
+control_group "Oracle Audit" do
+  
+  control 'Oracle' do
+    it 'should be listening on port 5500' do
+      expect(port(5500)).to be_listening
+    end
 
-  describe port(5500) do
-    it { should be_listening }
-  end
+    it 'should be listening on port 1521' do
+      expect(port(1521)).to be_listening
+    end
 
-  describe port(1521) do
-    it { should be_listening }
+    it 'should be running' do
+      expect(service('oracle')).to be_running
+    end
   end
 end
