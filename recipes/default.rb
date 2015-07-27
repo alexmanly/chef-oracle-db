d8a4621880b55d08aa89a0fb05fe89a95b00357b
alexmanly <am@chef.io>
2015-07-27 09:49:00 +0000

    fixed flyway url

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,16 +3,16 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-# include_recipe 'base-oracle-db::setup-prereqs'
-# include_recipe 'oracle::default'
-# include_recipe 'oracle::logrotate_alert_log'
-# include_recipe 'oracle::logrotate_listener'
-# node[:oracle][:rdbms][:dbs].each do | dbs_name, bool |
-#   log "testing for directory: #{node[:oracle][:ora_base]}/admin/#{dbs_name}"
-#   unless ::File.directory?("#{node[:oracle][:ora_base]}/admin/#{dbs_name}")
-#     include_recipe 'oracle::createdb'
-#   end
-# end
+include_recipe 'base-oracle-db::setup-prereqs'
+include_recipe 'oracle::default'
+include_recipe 'oracle::logrotate_alert_log'
+include_recipe 'oracle::logrotate_listener'
+node[:oracle][:rdbms][:dbs].each do | dbs_name, bool |
+  log "testing for directory: #{node[:oracle][:ora_base]}/admin/#{dbs_name}"
+  unless ::File.directory?("#{node[:oracle][:ora_base]}/admin/#{dbs_name}")
+    include_recipe 'oracle::createdb'
+  end
+end
 include_recipe 'base-oracle-db::create-user-db'
 include_recipe 'base-oracle-db::execute-db-scripts'
 include_recipe 'base-oracle-db::audit-oracle'
